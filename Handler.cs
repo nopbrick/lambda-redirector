8e8cd74f10e9e8c282d891a10af200e85a91198b
Filip Ceglik <fceglik@gmail.com>
2022-02-15 18:43:43 +0100

    done; working

--- Handler.cs
+++ Handler.cs
@@ -7,7 +7,7 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace Redirector
 {
@@ -26,22 +26,35 @@ namespace Redirector
 
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
+            string getParams = "?";
+            if (request.QueryStringParameters != null)
+            {
+                foreach (var param in request.QueryStringParameters)
+                {
+                    getParams += param.Key;
+                    getParams += "=";
+                    getParams += param.Value;
+                    getParams += "&";
+                }
+            }
             var teamserver = Environment.GetEnvironmentVariable("teamserver");
-            var uri = new Uri("https://" + teamserver + "/" + request.RequestContext.Stage + request.Path);
+            var uri = new Uri("https://" + teamserver + "/" + request.RequestContext.Stage + request.Path + getParams);
+
+
             
             var newRequest = new HttpRequestMessage(new HttpMethod(request.HttpMethod), uri);
             newRequest.Method = new HttpMethod(request.HttpMethod);
             if (bodyMethods.Any(x => x.Contains(request.HttpMethod)))
             {
+                System.Console.WriteLine("BODY REQUEST");
                 newRequest.Content = new StringContent(request.Body);
             }
 
             foreach (var header in request.Headers)
             {
-                //Console.WriteLine($"{header.Key}, {header.Value}");
+                Console.WriteLine($"{header.Key}, {header.Value}");
                 newRequest.Headers.TryAddWithoutValidation(header.Key, header.Value);
-            }
-            
+            }            
             
             Console.WriteLine(uri.ToString());
             var response = await Client.SendAsync(newRequest);
@@ -50,14 +63,17 @@ namespace Redirector
             {
                 outBoundHeaders.TryAdd(header.Key,header.Value.FirstOrDefault());
             }
-
+                      
+            
             return new APIGatewayProxyResponse
             {
                 StatusCode = Convert.ToInt32(response.StatusCode),
-                Body = response.Content.ToString(),
+                Body = await response.Content.ReadAsStringAsync(),
                 Headers = outBoundHeaders
+
             };
+        
+            
         }
     }
 }
-
